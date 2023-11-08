574c2484c8bd322a7b303bba47851772fcfd2d70
Tom Robinson <tarobins@gmail.com>
2023-11-07 20:50:30 -0800

    Create package

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tictactoe',
+    version='0.0.1',
+
+    url='https://github.com/tarobins/tictactoe.git',
+    author='Tom Robinson ',
+    author_email='t.arobins@gmail.com',
+
+    py_modules=['tictactoe'],
+)
