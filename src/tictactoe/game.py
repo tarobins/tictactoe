3b698af622a1a43c4f7276ed0036ae4830b17ba0
Tom Robinson <tarobins@gmail.com>
2023-11-12 14:56:46 -0800

    Penalty for picking used space

--- src/tictactoe/game.py
+++ src/tictactoe/game.py
@@ -4,14 +4,21 @@ class Game:
     def __init__(self):
         self.board = Board()
         self.turn = 'X'
+        self.penalized_player = None
 
     def play(self, row, col):
+        if self.penalized_player is not None or self.board[row, col] is not None:
+            self.penalized_player = self.turn
+            self.turn = 'X' if self.turn == 'O' else 'O'
+            return 'E'
         self.board[row, col] = self.turn
         self.turn = 'X' if self.turn == 'O' else 'O'
         return self.board.winner()
 
     def value(self, player):
-        if self.board.winner() == player:
+        if self.penalized_player == player:
+            return -100
+        elif self.board.winner() == player:
             return 10
         elif self.board.winner() == 'C':
             return 5
--- tests/test_game.py
+++ tests/test_game.py
@@ -24,3 +24,36 @@ def test_game_play():
     assert g.value('X') == 10
     assert g.value('O') == -10
 
+def test_catsgame():
+    g = Game()
+    assert g.turn == 'X'
+    assert g.play(0,0) is None
+    assert g.turn == 'O'
+    assert g.play(0,1) is None
+    assert g.turn == 'X'
+    assert g.play(0,2) is None
+    assert g.turn == 'O'
+    assert g.play(1,0) is None
+    assert g.turn == 'X'
+    assert g.play(1,1) is None
+    assert g.turn == 'O'
+    assert g.play(2,0) is None
+    assert g.turn == 'X'
+    assert g.play(1,2) is None
+    assert g.turn == 'O'
+    assert g.play(2,2) is None
+    assert g.turn == 'X'
+    assert g.play(2,1) == 'C'
+    assert g.value('X') == 5
+    assert g.value('O') == 5
+
+def test_badplay_x():
+    g = Game()
+    assert g.turn == 'X'
+    assert g.play(0,0) is None
+    assert g.turn == 'O'
+    assert g.play(0,0) == 'E'
+    assert g.turn == 'X'
+    assert g.play(0,1) is 'E'
+    assert g.value('X') == -100
+    assert g.value('O') == 0
