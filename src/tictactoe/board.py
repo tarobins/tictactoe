4ddb6ae102814f9e0526e2b1d1434141164dcd10
Tom Robinson <tarobins@gmail.com>
2023-11-19 20:59:42 -0800

    Convert to internal int rep

--- src/tictactoe/board.py
+++ src/tictactoe/board.py
@@ -2,27 +2,36 @@ import numpy as np
 
 class Board:
     def __init__(self):
-        self._board = np.array([[None, None, None], [None, None, None], [None, None, None]])
+        self._board = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
 
     def __str__(self):
-        return str(self._board)
+        return str(np.array(self, dtype=object))
 
     def __repr__(self):
-        return str(self._board)
+        return str(np.array(self, dtype=object))
     
     def __getitem__(self, key):
-        return self._board[key[0]][key[1]]
-    
+        if self._board[key[0]][key[1]] == 1:
+            return 'X'
+        elif self._board[key[0]][key[1]] == -1:
+            return 'O'
+        else:
+            return None
+        
     def __setitem__(self, key, value):
-        self._board[key[0]][key[1]] = value
+        if value == 'X':
+            self._board[key[0]][key[1]] = 1
+        elif value == 'O':
+            self._board[key[0]][key[1]] = -1
 
     def __array__(self, dtype=object):
-        board = self._board.copy()
-        if np.issubdtype(dtype, np.number):
-            board[board == 'X'] = 1
-            board[board == 'O'] = -1
-            board[board == None] = 0
-        return board.astype(dtype)
+        if not np.issubdtype(dtype, np.number):
+            board = self._board.astype(object)
+            board[board == 1] = 'X'
+            board[board == -1] = 'O'
+            board[board == 0] = None
+            return board
+        return self._board.astype(dtype)
     
     def winner(self):
 
@@ -31,22 +40,30 @@ class Board:
         # Check for horizontal wins.
         for row in range(3):
             if self._board[row][0] == self._board[row][1] == self._board[row][2] and self._board[row][0] is not None:
-                return self._board[row][0]
+                return _to_name(self._board[row][0])
 
         # Check for vertical wins.
         for col in range(3):
             if self._board[0][col] == self._board[1][col] == self._board[2][col] and self._board[0][col] is not None:
-                return self._board[0][col]
+                return _to_name(self._board[0][col])
 
         # Check for diagonal wins.
         if self._board[0][0] == self._board[1][1] == self._board[2][2] and self._board[0][0] is not None:
-            return self._board[0][0]
+            return _to_name(self._board[0][0])
         elif self._board[0][2] == self._board[1][1] == self._board[2][0] and self._board[0][2] is not None:
-            return self._board[0][2]
+            return _to_name(self._board[0][2])
         
         # Check for cat's game.
-        if not None in self._board:
+        if not 0 in self._board:
             return 'C'
 
         # If there are no winners, return None.
+        return None
+
+def _to_name(int_rep):
+    if int_rep == 1:
+        return 'X'
+    elif int_rep == -1:
+        return 'O'
+    else:
         return None
